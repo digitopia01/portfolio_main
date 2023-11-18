c3f60f529954edf9372afa218c3926a7f832b25e
dietscience <140796008+digitopia01@users.noreply.github.com>
2023-11-18 16:09:34 +0900

    SCSS 변수 지정 사용 및 HTML, CSS W3C Validator 로 검사 후 최적화

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ import { WaveGroup } from "./wavegroup.js";
 
 const themes = document.querySelectorAll(".theme");
 
-function moveup() {
-  themes.forEach((item) => {
-    item.onmouseover = function () {
-      item.style.transform = `translateY(-30px)`;
-    };
-    item.onmouseleave = function () {
-      item.style.transform = `translateY(0px)`;
-    };
-  });
-}
-moveup();
+// function moveup() {
+//   themes.forEach((item) => {
+//     item.onmouseover = function () {
+//       item.style.transform = `translateY(-30px)`;
+//     };
+//     item.onmouseleave = function () {
+//       item.style.transform = `translateY(0px)`;
+//     };
+//   });
+// }
+// moveup();
 
 window.onload = () => {
   new App();
