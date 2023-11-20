905e4ac2bda110fe3a2d2e349289acaa007b6a04
dietscience <140796008+digitopia01@users.noreply.github.com>
2023-11-20 12:02:16 +0900

    23-11-20 `visual`페이지 구성을 전체적으로 처음 구상한 것과 비슷하게 구성, README.MD 업데이트

--- README.md
+++ README.md
@@ -80,3 +80,20 @@
 <img src="img/forreadme12.png">
 <img src="img/forreadme13.png">
 <img src="img/forreadme14.png">
+
+#### 2023-11-19
+
+- `visual` 페이지가 마음에 안들어서 코드 수정
+
+<img src="img/forreadme15.png">
+
+### Agenda
+
+- visual 테마 컬러 클릭 시 변경되도록 설정
+- scroll event 플러그인 찾아서 적용하기
+- con2 내용 전체적으로 변경할 필요 있음
+  - Design Tools 의 경우 이미지를 넣는 것이 어떨지 구상해보기
+- con3 hover시 이벤트 js로 만들어보기
+  - 상하좌우 어떤 방향에서 들어오든 그 방향에서 background 컬러 생성되도록
+  - con3 유닛 클릭 시 모달창으로 프로젝트 상세페이지 나오도록 구성
+- con4 thumnail random color 로 구성되도록 짜기 rbga 를 사용하는 것이 좋을 것 같음
--- app.js
+++ app.js
@@ -1,65 +1,96 @@
+class Point {
+  constructor(index, x, y, waveIndex) {
+    this.x = x;
+    this.y = y;
+    this.fixedY = canvas.height / 2; // y값을 canvas의 가운데로 고정
+    this.speed = 0.01;
+    // 초기 타이밍에 딜레이 추가
+    this.cur = index * 5 + waveIndex * 200; // 각 파도별로 포인트들이 최소 차이 10px로 다른 타이밍에 움직이게 설정
+    this.max = Math.random() * 100 + 30;
+
+    // 고정된 컬러 사용
+    const colors = [
+      "rgba(255,0,0,0.7)",
+      "rgba(255,255,0,0.7)",
+      "rgba(0,0,255,0.7)",
+    ];
+    this.color = colors[waveIndex];
+  }
+
+  update() {
+    this.cur += this.speed;
+    this.y = this.fixedY + Math.sin(this.cur) * this.max;
+  }
+}
+
 const canvas = document.getElementById("waveCanvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const waves = [
-  {
-    amplitude: 50,
-    frequency: 0.01,
-    phaseX: 0,
-    phaseY: 0,
-    color: "rgba(255, 55, 75, 0.7)",
-  },
-  {
-    amplitude: 30,
-    frequency: 0.015,
-    phaseX: Math.PI / 4,
-    phaseY: Math.PI / 4,
-    color: "rgba(30, 100, 255, 0.7)",
-  },
-  {
-    amplitude: 20,
-    frequency: 0.02,
-    phaseX: Math.PI / 2,
-    phaseY: Math.PI / 2,
-    color: "rgba(255, 200, 50, 0.7)",
-  },
-];
+const waves = [];
 
-function drawWave() {
+for (let j = 0; j < 3; j++) {
+  const points = [];
+  for (let i = 0; i < 9; i++) {
+    points.push(new Point(i, (canvas.width / 5) * i, canvas.height / 2, j));
+  }
+  waves.push(points);
+}
+
+function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  waves.forEach((wave) => {
-    ctx.beginPath();
-    ctx.moveTo(0, canvas.height / 3);
+  waves.forEach((points) => {
+    points.forEach((point) => {
+      point.update();
+    });
 
-    for (let i = 0; i < canvas.width; i++) {
-      const x = wave.amplitude * Math.sin(wave.frequency * i + wave.phaseX) + i;
-      const y =
-        wave.amplitude * Math.sin(wave.frequency * i + wave.phaseY) +
-        canvas.height / 2.8;
-      ctx.quadraticCurveTo(x - 1, y, x, y);
+    ctx.beginPath();
+
+    // Set the starting point at the left edge of the canvas
+    ctx.moveTo(0, points[0].y);
+
+    for (let i = 1; i < points.length - 2; i++) {
+      const xc = (points[i].x + points[i + 1].x) / 2;
+      const yc = (points[i].y + points[i + 1].y) / 2;
+      ctx.quadraticCurveTo(points[i].x, points[i].y, xc, yc);
     }
 
+    ctx.quadraticCurveTo(
+      points[points.length - 2].x,
+      points[points.length - 2].y,
+      points[points.length - 1].x,
+      points[points.length - 1].y
+    );
+
+    // Set the ending point at the right edge of the canvas
+    ctx.lineTo(canvas.width, points[points.length - 1].y);
     ctx.lineTo(canvas.width, canvas.height);
     ctx.lineTo(0, canvas.height);
     ctx.closePath();
-    ctx.fillStyle = wave.color;
+    ctx.fillStyle = points[0].color; // 파도의 첫 번째 포인트의 색상 사용
     ctx.fill();
-
-    // 웨이브 이동
-    wave.phaseX += 0.01;
-    wave.phaseY += 0.02;
   });
 
-  requestAnimationFrame(drawWave);
+  requestAnimationFrame(draw);
 }
 
 window.addEventListener("resize", () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+
+  waves.forEach((points, j) => {
+    points.forEach((point, index) => {
+      point.x = (canvas.width / 5) * index;
+      point.y = canvas.height / 2;
+      point.fixedY = canvas.height / 2;
+      point.max = Math.random() * 100 + 150;
+      // 딜레이에 따라 초기 타이밍 조절
+      point.cur = index * 5 + j * 10;
+    });
+  });
 });
 
-drawWave();
+draw();
